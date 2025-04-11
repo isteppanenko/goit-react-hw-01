72716a0ddb475701451a8cf3b1327e109cce4bcf
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2025-04-11 07:41:54 +0200

    fix_app_component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,8 @@ import Profile from './Profile/Profile.jsx';
 import userData from '../assets/userData.json';
 import friendList from '../assets/friendsList.json';
 import FriendList from './FriendList/FriendList.jsx';
-import FriendListItem from './FriendListItem/FriendListItem.jsx';
 import TransactionHistory from './TransactionHistory/TransactionHistory.jsx';
 import transactions from '../assets/transactionsHistory.json';
-import friends from '../assets/friendsList.json';
 
 // console.log(friendList);
 
@@ -22,7 +20,7 @@ const App = () => {
         followers={userData.stats.followers}
         views={userData.stats}
       />
-      <FriendList friends={friends} />
+      <FriendList friends={friendList} />
       <TransactionHistory transactions={transactions} />
     </>
   );
